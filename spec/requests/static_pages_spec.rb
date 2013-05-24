9c54dd1ca9c8e55f8b7bad1ea6cc072ee0946f3d
Jeremy Gaudet <jeremysgaudet@gmail.com>
2013-05-24 11:03:34 -0400

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+	describe "home page" do
+		
+		it "should have h1 content 'Sample App'" do
+			visit '/static_pages/home'
+			page.should have_selector('h1', :text => "Sample App")
+		end
+
+		it "should have title 'Home'" do
+			visit '/static_pages/home'
+			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+		end
+
+	end
+
+	describe "help page" do
+		
+		it "should have h1 content 'Help'" do
+			visit '/static_pages/help'
+			page.should have_selector('h1', :text => "Help")
+		end
+
+		it "should have title 'Help'" do
+			visit '/static_pages/help'
+			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+		end
+
+	end
+
+	describe "about page" do
+
+		it "should have h1 content 'About'" do
+			visit '/static_pages/about'
+			page.should have_selector('h1', :text => "About")
+		end
+
+		it "should have title 'About'" do
+			visit '/static_pages/about'
+			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About")
+		end
+
+	end
+
+end
